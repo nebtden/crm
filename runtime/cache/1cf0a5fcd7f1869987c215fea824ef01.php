47f1ad2f9f88aa22e2e70115eae8172c1bdaab58
zhangzhen <278203533@qq.com>
2020-05-25 16:05:04 +0800

    first

---
+++ runtime/cache/1cf0a5fcd7f1869987c215fea824ef01.php
@@ -0,0 +1,4 @@
+<?php
+//000000259200
+ exit();?>
+a:8:{s:2:"id";i:1;s:8:"username";s:5:"admin";s:3:"img";s:0:"";s:9:"thumb_img";s:0:"";s:8:"realname";s:0:"";s:9:"parent_id";i:0;s:14:"structure_name";N;s:12:"structure_id";N;}
---
+++ runtime/cache/2fea04905151dac3a4db6b0b84d2a112.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+s:120:"/Users/nbin/Documents/workSpace/workSpace-shenque/workSpace-php/72crm/runtime/cache/1cf0a5fcd7f1869987c215fea824ef01.php";
